b01e7539e66609570a551660a9ab1d40fd644b42
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2022-09-21 14:12:14 +0200

    testing pointing straight at ind site

--- js/results-component.js
+++ js/results-component.js
@@ -45,25 +45,13 @@ export class ResultsComponent extends HTMLElement {
 
 	getAllAvailableAppointments = async () => {
 		const buildURL = (locationName) => {
-			const locationKey = listLocations.find(
-				(location) => location.name === locationName
-			);
-			return `https://oap.ind.nl/oap/api/desks/${locationKey}/slots/?productKey=${this.api}&persons=${this.savedPersons}`;
+			return `https://ind-checker-server.everlearning.com.au/${this.api}/${locationName}/${this.savedPersons}`;
 		};
-
-		// const buildURL = (locationName) => {
-		// 	return `https://ind-checker-server.everlearning.com.au/${this.api}/${locationName}/${this.savedPersons}`;
-		// };
 
 		const fetchData = async (url) => {
 			const response = await fetch(url, {
 				method: "GET",
 			}).then((resp) => resp.text());
-			let respStart = "{";
-
-			const parts = response.body.split(')]}\',\n{"status":"OK",');
-			// res.json(JSON.parse((respStart += parts[1])).data);
-			console.log({ parts });
 			return JSON.parse(response);
 		};
 
