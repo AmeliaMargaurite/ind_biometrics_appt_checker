2b5473130d7ab479ebbf58803e7687f6cae94fe2
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2023-01-09 14:12:41 +0100

    added grab for posthog toolbar

--- js/posthog.js
+++ js/posthog.js
@@ -58,3 +58,10 @@ posthog.init("phc_zDkzQ5TNwVlhTTCaE53xdl5IwdaiQgc3PWbTQY6xSFR", {
 	},
 	persistence: "memory",
 });
+
+const toolbarJSON = new URLSearchParams(window.location.hash.substring(1)).get(
+	"__posthog"
+);
+if (toolbarJSON) {
+	posthog.loadToolbar(JSON.parse(toolbarJSON));
+}
